c5f8daf996a169a8ac2ac0622f9c3254720eaff4
Adam oudaimah <adam@800storagedubai.com>
2023-11-19 16:33:40 +0100

    deploy

--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import { HiDotsVertical } from "react-icons/hi";
 import { AiOutlineRise, AiOutlineFall } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 const DashBoard = () => {
+  const nav = useNavigate();
   const [rise, setRise] = useState(true);
   return (
     <div>
       <div className='dashboard-time-export mb-4 d-flex justify-content-between align-items-center'>
         <h1 className='mb-0  pe-3 py-3'>DashBoard</h1>
         <div className='d-flex rounded-3 gap-3'>
-          <select className='form-select' aria-label='Default select example'>
+          <select
+            className='form-select fs-5 fw-bold'
+            aria-label='Default select example'
+          >
             {/* <option>Open this select menu</option> */}
             <option value='1'>1 October 2022</option>
             <option value='2'>2 October 2022</option>
@@ -33,7 +38,12 @@ const DashBoard = () => {
         <div className='d-flex flex-column w-100 bg-white p-4 rounded-3'>
           <div className='d-flex w-100 justify-content-between align-items-center'>
             <h4>Total sells</h4>
-            <HiDotsVertical className='dots' />
+            <HiDotsVertical
+              className='dots'
+              // onClick={() => {
+              //   nav("/");
+              // }}
+            />
           </div>
           <div className='mt-4 d-flex w-100 justify-content-between align-items-center'>
             <h1>$100.00</h1>
